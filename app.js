c9aeea00aa16634b6ca41885f3c54f2d25c1cd47
Jack DesCombes <jpdesc@alumni.stanford.edu>
2023-02-10 14:23:05 -0800

    Start creating form

--- app.js
+++ app.js
@@ -9,27 +9,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const topArtists = {};
+const topArtistsArray = [];
 
 app.get("/", async function (req, res) {
+  res.render("index", { topArtistsArray: topArtistsArray });
+});
+
+app.post("/", async function (req, res) {
   const response = await getTopArtists();
   const { items } = await response.json();
   for (let idx in items) {
     let obj = items[idx];
     //   console.log(obj["name"]);
-
     topArtists[idx] = obj["name"];
-    let bandsintown;
   }
   let topArtistsArray = getTopArtistsArray(topArtists);
-
-  res.render("index", { topArtistsArray: topArtistsArray });
-
-  //   const artists = items.slice(0, 10).map((track) => ({
-  //     artist: track.artists.map((_artist) => _artist.name).join(", "),
-  //     songUrl: track.external_urls.spotify,
-  //     title: track.name,
-  //   }));
-  //   console.log(res);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
--- helpers.js
+++ helpers.js
@@ -10,8 +10,7 @@ const basic = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 
 const TOP_ARTISTS_BASE_ENDPOINT = `https://api.spotify.com/v1/me/top/artists/`;
-const TOP_ARTISTS_ENDPOINT =
-  TOP_ARTISTS_BASE_ENDPOINT + "?time_range=long_term&limit=50";
+// "?time_range=long_term&limit=50";
 
 const getAccessToken = async () => {
   const response = await fetch(TOKEN_ENDPOINT, {
@@ -28,8 +27,9 @@ const getAccessToken = async () => {
   return response.json();
 };
 
-export const getTopArtists = async () => {
+export const getTopArtists = async (time_range, limit) => {
   const { access_token } = await getAccessToken();
+  const TOP_ARTISTS_ENDPOINT = TOP_ARTISTS_BASE_ENDPOINT + time_range + limit;
   return fetch(TOP_ARTISTS_ENDPOINT, {
     headers: {
       Authorization: `Bearer ${access_token}`,
