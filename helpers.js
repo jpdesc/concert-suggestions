795ae1ef32f7e7062f900696f6f70a980cd4c1ee
Jack DesCombes <jpdesc@alumni.stanford.edu>
2023-02-13 16:39:19 -0800

    Set up ticketmaster api constants

--- helpers.js
+++ helpers.js
@@ -5,11 +5,16 @@ import querystring from "querystring";
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
 const refresh_token = process.env.REFRESH_TOKEN;
+const ticketmaster_api_key = process.env.TICKETMASTER_API_KEY;
 
 const basic = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 
 const TOP_ARTISTS_BASE_ENDPOINT = `https://api.spotify.com/v1/me/top/artists/`;
+const TICKETMASTER_BASE_ENDPOINT =
+  "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" +
+  ticketmaster_api_key;
+
 // "?time_range=long_term&limit=50";
 
 const getAccessToken = async () => {
